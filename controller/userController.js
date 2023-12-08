aae825fe49ac5ab37aca5c64d773209c31111b3d
DevinHansa <devinhansa@gmail.com>
2023-12-08 16:12:22 +0530

    Fifth commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,24 +1,54 @@
 const userSchema = require('../model/userSchema');
 const bcrypt = require('bcrypt');
 const salt = 10;
+const nodemailer = require('nodemailer');
 
 const register = (req, resp) => {
-    bcrypt.hash(req.body.password, salt, function (err, hash) {
-        if (err) {
-            return resp.status(500).json(err);
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hansa8109@gmail.com',
+            pass: 'qrsa gmmr letd ybey'
         }
-        const use = new userSchema({
-            fullName: req.body.fullName,
-            email: req.body.email,
-            password: hash,
-            activeStatus: req.body.activeStatus
-        });
+    });
 
-        use.save().then(saveResponse => {
-            return resp.status(201).json({ 'message': 'saved!' });
-        }).catch(error => {
-            return resp.status(500).json(err);
-        });
+    const mailOptions = {
+        from: 'hansa8109@gmail.com',
+        to: req.body.email,
+        subject: 'New account creation',
+        text: 'You have created your account'
+    };
+
+    userSchema.findOne({ 'email': req.body.email }).then(result => {
+        if (result) {
+            // Email already registered
+            return resp.status(409).json({ 'error': 'Email already registered' });
+        } else {
+            transporter.sendMail(mailOptions, function (error, info) {
+                if (error) {
+                    return resp.status(500).json({ 'error': error });
+                } else {
+                    bcrypt.hash(req.body.password, salt, function (err, hash) {
+                        if (err) {
+                            return resp.status(500).json(err);
+                        }
+                        const use = new userSchema({
+                            fullName: req.body.fullName,
+                            email: req.body.email,
+                            password: hash,
+                            activeStatus: req.body.activeStatus
+                        });
+
+                        use.save().then(saveResponse => {
+                            // Both email and user data saved successfully
+                            return resp.status(201).json({ 'message': 'saved!' });
+                        }).catch(error => {
+                            return resp.status(500).json(error);
+                        });
+                    });
+                }
+            });
+        }
     });
 }
 
@@ -30,3 +60,6 @@ module.exports = {
     register,
     login
 };
+
+
+
